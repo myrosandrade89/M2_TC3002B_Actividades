07f80e31c6af2b821861219a8d330fa8ca086057
Myroslava Sanchez Andrade <myrosandrade89@gmail.com>
2023-09-25 22:07:08 -0700

    Initial commit

---
+++ pandas/lesson1.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.DataFrame({'Yes': [50, 21], 'No': [131, 2]})
+
+print("DataFrames:")
+
+print(df)
+
+df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']})
+
+print(df)
+
+df = pd.DataFrame({'Bob': ['I liked it.', 'It was awful.'], 'Sue': ['Pretty good.', 'Bland.']}, index=['Product A', 'Product B'])
+
+print(df)
+
+print("Series:")
+
+s = pd.Series([1, 2, 3, 4, 5])
+
+print(s)
+
+s = pd.Series([30, 35, 40], index=['2015 Sales', '2016 Sales', '2017 Sales'], name='Product A')
+
+print(s)
+
+print("Reading data files:")
+
+wine_reviews = pd.read_csv("../wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+
+print(wine_reviews.shape)
+
+print(wine_reviews.head())
+
